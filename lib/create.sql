8695d6b3871d64b03da292892f925974c502652e
Adam Palmer <theadampalmer@gmail.com>
2020-08-06 04:45:28 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+-- CREATE TABLE bears (id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER,
+--     gender TEXT,
+--     color TEXT,
+--     temperament TEXT,
+--     alive BOOLEAN,
+-- );
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,'Mr. Chocolate', 30, 'male', 'brown', 'wild', 1),
+-- (2, 'Rowdy', 12, 'male', 'brown', 'wild', 1),
+-- (3, 'Tabitha', 25, 'female', 'black', 'playful', 1),
+-- (4, 'Sergeant Brown', 18, 'female', 'brown', 'moody', 1)
+-- (5,"Melissa", 13, "female", "dark brown", "goofy", 1),
+-- (6,"Grinch", 2, "male", "black", "unfriendly", 1),
+-- (7,"Wendy", 6, "female", "Blue", "playful", 1),
+-- (8, null, 20, "male", "black", "wicked", 0);
+
+INSERT INTO bears (name) VALUES ('Mr. Chocolate');
+INSERT INTO bears (name) VALUES ('Rowdy');
+INSERT INTO bears (name) VALUES ('Tabitha');
+INSERT INTO bears (name) VALUES ('Sergeant Brown');
+INSERT INTO bears (name) VALUES ('Melissa');
+INSERT INTO bears (name) VALUES ('Grinch');
+INSERT INTO bears (name) VALUES ('Wendy');
+INSERT INTO bears (name) VALUES (NULL);
