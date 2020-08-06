2bac7073468dbbd942584388cc2c0ffd7d2f22b3
Adam Palmer <theadampalmer@gmail.com>
2020-08-06 04:39:59 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age LIMIT 1"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1"
 end
 
 def select_youngest_bear_and_returns_name_and_age
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color FROM bears "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
