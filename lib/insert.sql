f8875e6fa4a0849d03b34ca0cad35aee4692844f
Adam Palmer <theadampalmer@gmail.com>
2020-08-06 04:31:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,'Mr. Chocolate', 30, 'male', 'brown', 'wild', 1),
-(2, 'Rowdy', 12, 'male', 'brown', 'wild', 1),
-(3, 'Tabitha', 25, 'female', 'black', 'playful', 1),
-(4, 'Sergeant Brown', 18, 'female', 'brown', 'moody', 1)
-(5,"Melissa", 13, "female", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "male", "black", "unfriendly", 1),
-(7,"Wendy", 6, "female", "Blue", "playful", 1),
-(8, null, 20, "male", "black", "wicked", 0);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,'Mr. Chocolate', 30, 'male', 'brown', 'wild', 1),
+-- (2, 'Rowdy', 12, 'male', 'brown', 'wild', 1),
+-- (3, 'Tabitha', 25, 'female', 'black', 'playful', 1),
+-- (4, 'Sergeant Brown', 18, 'female', 'brown', 'moody', 1)
+-- (5,"Melissa", 13, "female", "dark brown", "goofy", 1),
+-- (6,"Grinch", 2, "male", "black", "unfriendly", 1),
+-- (7,"Wendy", 6, "female", "Blue", "playful", 1),
+-- (8, null, 20, "male", "black", "wicked", 0);
